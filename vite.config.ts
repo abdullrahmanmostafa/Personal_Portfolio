6ebe1f981d131c9f937df47e14c77ac40823976f
Abdullrahman Mostafa <abdullrahmanmostafa@MacBook-Air-Abdullrahman.local>
2026-08-04 19:52:11 +0300

    save the files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Personal_Portfolio/',
+  base: '/',
+  publicDir: 'assets',
 
   plugins: [react(), tailwindcss()],
 
