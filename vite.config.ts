f68f10901e67ddf219d26708553182ef9445b1bb
Abdullrahman Mostafa <abdullrahmanmostafa@MacBook-Air-Abdullrahman.local>
2026-08-04 20:03:01 +0300

    revert to the old version

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,16 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-
 export default defineConfig({
-  base: '/',
-  publicDir: 'assets',
-
-  plugins: [react(), tailwindcss()],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+base: '/Personal_Portfolio/',
+plugins: [react(), tailwindcss()],
+resolve: {
+alias: {
+'@': path.resolve(__dirname, '.'),
     },
   },
-
-  server: {
-    hmr: process.env.DISABLE_HMR !== 'true',
-    watch: process.env.DISABLE_HMR === 'true' ? null : {},
+server: {
+hmr: process.env.DISABLE_HMR !== 'true',
+watch: process.env.DISABLE_HMR === 'true' ? null : {},
   },
 });
